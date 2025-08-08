0d36e2dc47056d20f6e3a3ee75355f27b17f09d3
smhrd <smhrd@192.168.219.161>
2025-08-08 11:01:30 +0900

    코드 수정

--- AjaxProject/src/main/java/com/smhrd/gittest/TestController.java
+++ AjaxProject/src/main/java/com/smhrd/gittest/TestController.java
@@ -5,8 +5,9 @@ public class TestController {
 
 	public static void main(String[] args) {
 		System.out.println("테스트 코드");
-		// 영빈
-		System.out.println("영빈");
+		
+		//B개발자
+		System.out.println("코드 수정");
 	}
 	
 		
